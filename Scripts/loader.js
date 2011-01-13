e968ef8a304ab25dd6388e7faf652f3447a904be
Yaroslaff Fedin <me@inviz.ru>
2011-01-13 22:31:52 +0800

    YAY. Github now fetches submodules. No need for custom loader. Triple hooray

--- Scripts/loader.js
+++ Scripts/loader.js
@@ -1,16 +1,5 @@
 window.prefix = '';
 if (location.host.indexOf('github') > -1 || location.host.indexOf('fiddle') > -1 || location.host.indexOf('inviz.ru') > -1) {
-  window.loader = function(src) {
-    src = src.replace(/^.*(lsd|lsd-base|lsd-examples|lsd-widgets|mootools-ext|mootools-speedups|mootools-mobile)\//, 'http://inviz.github.com/$1/').
-  	          replace(/^.*(qfocuser|cssparser)\//, 'http://github.com/inviz/$1/raw/master/').
-  	          replace(/^.*(..\/..\/..\/Source)\//, 'http://inviz.github.com/lsd/Source/').
-  	          replace(/^.*(..\/..\/Source)\//, 'http://inviz.github.com/lsd-examples/Source/').
-  	          replace(/^.*(mootools-core|mootools-more)\//, 'http://github.com/mootools/$1/raw/master/').
-            	replace(/^.*(mootools-color|art)\//, 'http://github.com/kamicane/$1/raw/master/')
-       
-    document.write('<scr' + 'ipt src="' + (prefix || '') + src + '"></sc' + 'ript>');
-  }
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-18508300-1']);
   _gaq.push(['_trackPageview']);
